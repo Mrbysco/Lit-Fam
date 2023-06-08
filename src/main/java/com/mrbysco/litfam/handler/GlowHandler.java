61a0d5855263b012250bb22885153802d03e7635
Mrbysco <Mrbysco@users.noreply.github.com>
2023-06-09 01:09:21 +0200

    Port to 1.20 [build] [publish]

--- src/main/java/com/mrbysco/litfam/handler/GlowHandler.java
+++ src/main/java/com/mrbysco/litfam/handler/GlowHandler.java
@@ -6,6 +6,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,12 +17,13 @@ public class GlowHandler {
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent event) {
 		Player player = event.player;
-		if (player != null && !player.isSpectator() && player.level.getGameTime() % 5 == 0 && LitConfig.COMMON.glowEnabled.get()) {
+		Level level = player.level();
+		if (player != null && !player.isSpectator() && level.getGameTime() % 5 == 0 && LitConfig.COMMON.glowEnabled.get()) {
 			final int range = LitConfig.COMMON.glowRange.get();
 			final TargetingConditions closePredicate = (TargetingConditions.forNonCombat()).range(range);
 			final AABB closeBox = player.getBoundingBox().inflate(range, 5.0D, range);
 
-			List<LivingEntity> closeEntities = player.level.getNearbyEntities(LivingEntity.class, closePredicate, player, closeBox);
+			List<LivingEntity> closeEntities = level.getNearbyEntities(LivingEntity.class, closePredicate, player, closeBox);
 			for (LivingEntity livingEntity : closeEntities) {
 				livingEntity.addEffect(new MobEffectInstance(MobEffects.GLOWING, 10, 0, true, false));
 			}
